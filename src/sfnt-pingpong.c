0f2247285259a75744d42f1da98e3678c282827f
Robert Stonehouse <rstonehouse@solarflare.com>
2012-06-19 12:40:10 +0100

    bug30581: On solaris recv with a zero length buffer doesn't block
    hg-sha:89bdcbd62b8f

--- src/sfnt-pingpong.c
+++ src/sfnt-pingpong.c
@@ -437,13 +437,18 @@ static void do_init(void)
 
 static void do_ping(int read_fd, int write_fd, int sz)
 {
-  int i, rc;
+  int i, rc, bufsz = sz;
   for( i = 0; i < cfg_n_pings; ++i ) {
     rc = do_send(write_fd, ppbuf, sz, 0);
     NT_TESTi3(rc, ==, sz);
   }
+#if defined(__sun__)
+  /* SFC bug 30581: On solaris recv with a zero length buffer doesn't block */
+  if( (fd_type == FDT_UDP) && (sz == 0) )
+    bufsz = 1;
+#endif
   for( i = 0; i < cfg_n_pongs; ++i ) {
-    rc = mux_recv(read_fd, ppbuf, sz, MSG_WAITALL);
+    rc = mux_recv(read_fd, ppbuf, bufsz, MSG_WAITALL);
     NT_TESTi3(rc, ==, sz);
   }
 }
@@ -451,9 +456,14 @@ static void do_ping(int read_fd, int write_fd, int sz)
 
 static void do_pong(int read_fd, int write_fd, int sz)
 {
-  int i, rc;
+  int i, rc, bufsz = sz;
+#if defined(__sun__)
+  /* SFC bug 30581: On solaris recv with a zero length buffer doesn't block */
+  if( (fd_type == FDT_UDP) && (sz == 0) )
+    bufsz = 1;
+#endif
   for( i = 0; i < cfg_n_pings; ++i ) {
-    rc = mux_recv(read_fd, ppbuf, sz, MSG_WAITALL);
+    rc = mux_recv(read_fd, ppbuf, bufsz, MSG_WAITALL);
     NT_TESTi3(rc, ==, sz);
   }
   for( i = 0; i < cfg_n_pongs; ++i ) {
@@ -791,7 +801,7 @@ static void do_pings(int ss, int read_fd, int write_fd, int msg_size,
   uint64_t start, stop;
   int i;
 
-  sfnt_sock_put_int(ss, iter + 1);
+  sfnt_sock_put_int(ss, iter + 1); /* +1 as initial ping  below */
   sfnt_sock_put_int(ss, msg_size);
 
   /* Touch to ensure resident. */
